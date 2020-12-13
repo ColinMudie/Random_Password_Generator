7d2f4e92e7cbedcd77f971eb8b67cb11e284dac1
Colin Mudie <csmudie1@gmail.com>
2020-12-13 17:21:06 -0500

    added an array for each character type

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+var upCase = ["Q","W","E","R","T","Y","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+var numCase = ["1","2","3","4","5","6","7","8","9","0"];
+var symCase = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'",'"',",","<",".",">","/","?"]
 
 // Write password to the #password input
 function writePassword() {
